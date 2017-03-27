db8cae8bb561370dd80947e213e23fb6c0e06581
Ricardo Dominguez <richard291190@gmail.com>
2017-03-27 04:20:28 +0200

    Funciones para corregir errores con git.

---
+++ TP Final/src/LaAmenazaMundial/CopiadorABin.java
@@ -0,0 +1,22 @@
+package LaAmenazaMundial;
+
+public class CopiadorABin {
+        
+	  public static void iniciar() {
+		  
+		  CopiarFicheros.copiar("fondo.jpg", "bin/fondo.jpg");
+		  CopiarFicheros.copiar("gano.jpg", "bin/fondo.jpg");
+		  CopiarFicheros.copiar("imagen.png", "bin/imagen.png");
+		  
+		  for(int i=0;i<10;i++){
+			  
+			  if(i<4) CopiarFicheros.copiar("colesterol"+i+".png", "bin/colesterol"+i+".png");
+			  
+			  if(i<6) CopiarFicheros.copiar("fondoMalo"+i+".jpg", "bin/fondoMalo"+i+".jpg");
+		  
+			  CopiarFicheros.copiar("anticuerpo"+i+".png", "bin/anticuerpo"+i+".png");
+			  CopiarFicheros.copiar("celula"+i+".png", "bin/celula"+i+".png");
+			  CopiarFicheros.copiar("virus"+i+".png", "bin/virus"+i+".png");
+		  }
+	  }
+}
